f66b6f860de74f935744c5c4e8b1244bca4a21de
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-11 11:57:24 +0700

    latihan 1

---
+++ JOBSHEET 6/src/Fc2Latihan1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Fc2Latihan1 {
+
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner (System.in);
+
+        //deklarasi 
+        int bil1, bil2, bil3;
+
+        System.out.println("Masukkan Bilangan Pertama : ");
+        bil1 = input25.nextInt();
+        System.out.println("Masukkan Bilangan Kedua : ");
+        bil2 = input25.nextInt();
+        System.out.println("Masukkan Bilangan Ketiga : ");
+        bil3 = input25.nextInt();
+        
+
+        //pemilihan
+    } 
+}
