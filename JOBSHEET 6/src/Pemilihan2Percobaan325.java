c8ed491f16dcfee5bd7785e711ba3cc57cad73ea
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-11 11:25:31 +0700

    ubah equals

--- JOBSHEET 6/src/Pemilihan2Percobaan325.java
+++ JOBSHEET 6/src/Pemilihan2Percobaan325.java
@@ -16,7 +16,7 @@ public class Pemilihan2Percobaan325 {
         penghasilan = input25.nextInt();
 
         //pemilihan
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000)
                 pajak = 0.1;
             else if (penghasilan <= 3000000)
@@ -25,7 +25,7 @@ public class Pemilihan2Percobaan325 {
                 pajak = 0.2;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.print("Penghasil Bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
