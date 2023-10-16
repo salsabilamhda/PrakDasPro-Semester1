c8221387f93417397fe4a58855d7d4b4a0854985
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-16 12:11:12 +0700

    rata rata fix

--- JOBSHEET 7/src/ForKelipatan25.java
+++ JOBSHEET 7/src/ForKelipatan25.java
@@ -12,17 +12,21 @@ public class ForKelipatan25 {
         //perulangan
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
-                total += 1;
+                total += i;
                 counter++;
                 rataRata = (double) total / counter;
                
             }
         }
 
-        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
-        System.out.printf("Rata - rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
+    
+            System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+            System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+            System.out.printf("Rata - rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
 
+        
+
+        
 
     }
 }
