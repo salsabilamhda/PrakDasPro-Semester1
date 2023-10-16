270f47abfbb7f09bce795c11b9135b8698453ca5
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-16 12:00:46 +0700

    rata rata pertama

--- JOBSHEET 7/src/ForKelipatan25.java
+++ JOBSHEET 7/src/ForKelipatan25.java
@@ -5,6 +5,7 @@ public class ForKelipatan25 {
         Scanner scan25 = new Scanner (System.in);
 
         int kelipatan, total = 0, counter = 0;
+        double rataRata = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan25.nextInt();
 
@@ -13,11 +14,15 @@ public class ForKelipatan25 {
             if (i % kelipatan == 0) {
                 total += 1;
                 counter++;
+                rataRata = (double) total / counter;
+               
             }
         }
 
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata - rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
+
 
     }
 }
