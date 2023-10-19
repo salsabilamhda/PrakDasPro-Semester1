a2cc4f74f759c98b0f9dbd9271f225b50a99224a
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-19 13:57:03 +0700

    UTS

---
+++ UTS/UtsNimGanjil.java
@@ -0,0 +1,57 @@
+// Nama : Salsabila Mahda Runisha 
+// NIM : 2341720257
+import java.util.Scanner;
+public class UtsNimGanjil {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double totalHarga = 0;
+        int jumlahMinuman = 0, jumlahBarang = 0;
+    
+
+        while (true) {
+            System.out.print("Masukkan jenis barang (minuman/makanan): ");
+            String jenisBarang = input.nextLine();
+            if (jenisBarang.equalsIgnoreCase("selesai")) {
+                break;
+            }
+            System.out.print("Masukkan Nama Barang: ");
+            String namaBarang = input.nextLine();
+
+            System.out.print("Masukkan harga barang: ");
+            double hargaBarang = input.nextDouble();
+            input.nextLine(); 
+
+            if (jenisBarang.equalsIgnoreCase("minuman")) {
+                jumlahMinuman++;
+                if (jumlahMinuman == 3) {
+                    totalHarga = (totalHarga - (0.05 * hargaBarang));
+                } else {
+                    totalHarga += hargaBarang;
+                }
+            } else {
+                totalHarga += hargaBarang;
+            }
+        
+        }
+
+        System.out.print("Mau beli kantong plastik (ya/tidak)? ");
+        String beliKantongPlastik = input.nextLine();
+
+        if (beliKantongPlastik.equalsIgnoreCase("ya")) {
+            totalHarga += 200;
+        }
+
+        if (totalHarga >= 1000000) {
+            double ppn = totalHarga * 0.07;
+            totalHarga = totalHarga + ppn;
+        }
+
+        System.out.println("Total biaya yang harus dibayarkan: Rp. " + totalHarga);
+
+        input.close();
+    }
+}
+
+    
+
