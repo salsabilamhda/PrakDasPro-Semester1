9c2d2654c0057da397ec177106859e030f552298
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-24 19:46:17 +0700

    lulus dan tidak lulus

--- JOBSHEET 9/src/ArrayNilai25.java
+++ JOBSHEET 9/src/ArrayNilai25.java
@@ -8,13 +8,18 @@ public class ArrayNilai25 {
         int [] nilaiAkhir = new int [10];
 
         //perulangan
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = scan25.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }  else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }
+            
         }
     }
 }
