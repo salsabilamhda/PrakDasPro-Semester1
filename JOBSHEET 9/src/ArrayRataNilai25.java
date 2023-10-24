21c36710b294b91d2ce1e687ea47f46f91c6104e
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-24 19:56:51 +0700

    modifikasi 1

--- JOBSHEET 9/src/ArrayRataNilai25.java
+++ JOBSHEET 9/src/ArrayRataNilai25.java
@@ -4,13 +4,19 @@ public class ArrayRataNilai25 {
     public static void main(String[] args) {
         Scanner scan25 = new Scanner (System.in);
 
+    
         int[] nilaiMhs= new int[10];
         double total = 0;
         double rata2;
+        int jumlahLulus = 0; //modifikasi
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = scan25.nextInt();
+
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus++; //modifikasi
+            }
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
@@ -19,5 +25,6 @@ public class ArrayRataNilai25 {
 
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Jumlah mahasiswa yang lulus: "+jumlahLulus); //modifikasi
     }
 }
