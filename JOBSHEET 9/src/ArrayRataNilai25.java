9acd9b8693bca130a6e8d87be209eec5197cf8ef
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-24 20:22:03 +0700

    inputan

--- JOBSHEET 9/src/ArrayRataNilai25.java
+++ JOBSHEET 9/src/ArrayRataNilai25.java
@@ -4,27 +4,45 @@ public class ArrayRataNilai25 {
     public static void main(String[] args) {
         Scanner scan25 = new Scanner (System.in);
 
-    
-        int[] nilaiMhs= new int[10];
-        double total = 0;
-        double rata2;
-        int jumlahLulus = 0; //modifikasi
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = scan25.nextInt(); //modifikasi2
 
+        int[] nilaiMhs= new int[jumlahMahasiswa]; //modifikasi2
+        double totalLulus = 0; //modifikasi2
+        double totalTidakLulus = 0; //modifikasi2
+        double rataLulus = 0; //modifikasi 2
+        double rataTidakLulus = 0; // modifikasi2
+        int jumlahLulus = 0; //modifikasi1
+        
+
+       
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = scan25.nextInt();
 
+
             if (nilaiMhs[i] > 70) {
-                jumlahLulus++; //modifikasi
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
             }
         }
+    
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+        if (jumlahLulus > 0) {
+            rataLulus = totalLulus / jumlahLulus;
+            System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        } else {
+            System.out.println("Tidak ada mahasiswa yang lulus.");
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-        System.out.println("Jumlah mahasiswa yang lulus: "+jumlahLulus); //modifikasi
+        if (jumlahMahasiswa - jumlahLulus > 0) {
+            rataTidakLulus = totalTidakLulus / (jumlahMahasiswa - jumlahLulus);
+            System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+        } else {
+            System.out.println("Tidak ada mahasiswa yang tidak lulus.");
+        }
+      
     }
 }
