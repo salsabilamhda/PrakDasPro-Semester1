6435440defc3f86c2c68bc6f4b42635b8c92e804
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-10-25 17:33:02 +0700

    modifikasi 2

--- JOBSHEET 9/src/ArrayBilangan25.java
+++ JOBSHEET 9/src/ArrayBilangan25.java
@@ -6,10 +6,9 @@ public class ArrayBilangan25 {
         
                 int[] bil = {5, 13, -7, 17};
         
-                System.out.println(bil[0]);
-                System.out.println(bil[1]);
-                System.out.println(bil[2]);
-                System.out.println(bil[3]);
+                for (int i = 0; i < 4; i++) {
+                    System.out.println(bil[i]);
+                }
             }
         }
         
