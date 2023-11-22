7764c1828189d38e11e19675526080bf5a0423ac
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2023-11-22 19:04:57 +0700

    percobaan 6 dengan fungsi

--- JOBSHEET 13/src/Percobaan625.java
+++ JOBSHEET 13/src/Percobaan625.java
@@ -2,22 +2,31 @@ import java.util.Scanner;
 
 public class Percobaan625 {
     
+    static int hitungLuas (int pjg, int lb) {
+        int Luas=pjg*lb;
+        return Luas;
+    }
+
+    static int hitungVolume (int tinggi, int a, int b) {
+        int volume= hitungLuas(a,b)*tinggi;
+        return volume;
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        int p, l, t, L, vol;
-
-        System.out.println("Masukkan panjang");
+        Scanner input = new Scanner (System.in);
+        int p,l,t,L, vol;
+         System.out.println("Masukkan panjang");
         p=input.nextInt();
         System.out.println("Masukkan lebar");
         l=input.nextInt();
         System.out.println("Masukkan tinggi");
         t=input.nextInt();
         
-        L=p*l;
+        L=hitungLuas(p,l);
         System.out.println("Luas Persebgi panjang adalah"+L);
 
-        vol=p*l*t;
+        vol=hitungVolume(t, p,l);
         System.out.println("Volume balok adalah"+vol);
     }
 }
+
